76cb4015170ed10004e30cfdc95cc6ee6cf53026
cuad0ng <thanhphuoc01012001@gmail.com>
2022-07-01 18:22:40 +0700

    init project

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+const server = http.createServer((http.request, http.response) -> {
+    console.log('run request ...')
+    res.setHeader('Content-Type', 'text/html')
+    res.wr
+})
